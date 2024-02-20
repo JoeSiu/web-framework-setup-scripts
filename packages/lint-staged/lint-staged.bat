2adde3a9fd3e5a6c5bb7b51ea1b2534df983a660
JoeSiu <joesiu.dev@gmail.com>
2024-02-21 00:29:00 +0800

    fix: remove double quote from echo

--- packages/lint-staged/lint-staged.bat
+++ packages/lint-staged/lint-staged.bat
@@ -2,7 +2,7 @@
 echo [7mDocumentation[0m https://github.com/lint-staged/lint-staged
 call npm install --save-dev lint-staged
 echo Create lint-staged.config.js
-echo "module.exports = { '*': ['echo'] };" > lint-staged.config.js
+echo module.exports = { '*': ['echo'] }; > lint-staged.config.js
 echo.
 echo Please install husty, then run add-lint-staged-hook.bat
 pause
