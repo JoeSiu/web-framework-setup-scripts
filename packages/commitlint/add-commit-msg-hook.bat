f539614d22ceeceb0e54002c5af5f73489107f86
JoeSiu <joesiu.dev@gmail.com>
2024-02-21 00:52:33 +0800

    fix: update commitlint hook

--- packages/commitlint/add-commit-msg-hook.bat
+++ packages/commitlint/add-commit-msg-hook.bat
@@ -3,7 +3,7 @@ echo [7mDocumentation[0m https://commitlint.js.org/#/guides-local-setup?id=usi
 echo Make sure to install commitlint and husty first
 echo.
 echo Add commit message linting to commit-msg hook
-echo npx --no -- commitlint --edit \$1 > .husky/commit-msg
+echo npx --no -- commitlint --edit ${1} > .husky/commit-msg
 echo.
 echo Done
 echo Alternatively, you can create a script inside package.json, see https://commitlint.js.org/#/guides-local-setup?id=using-a-git-hooks-manager for detail
